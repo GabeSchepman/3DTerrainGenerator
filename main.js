183db47bf4eed9dc4267c73907a47a945a58cf67
Gabe <gabriel.schepman@gmail.com>
2024-04-08 20:48:16 -0700

    Updates

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+const canvas = document.querySelector('.webgl');
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: canvas
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 camera.position.setZ(30);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
@@ -24,9 +25,9 @@ const controls = new OrbitControls(camera, renderer.domElement);
 function animate(){
   requestAnimationFrame(animate);
 
-  controls.update()
+  controls.update();
 
   renderer.render(scene, camera);
 }
 
-animate()
+animate();
