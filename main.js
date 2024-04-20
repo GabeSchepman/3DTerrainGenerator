b3ed976716ee55a2e9523bb28dff0d5d9fa6217b
Gabe <gabriel.schepman@gmail.com>
2024-04-19 21:31:42 -0700

    Updates

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
-//import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-const canvas = document.querySelector('.webgl');
+const canvas = document.getElementById('bg');
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  antialias: true
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
 camera.position.setZ(30);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const geometry = new THREE.PlaneGeometry(500, 500, 50, 50);
 const material = new THREE.MeshBasicMaterial({color: 0xFF4763, wireframe: true});
-const torus = new THREE.Mesh(geometry, material);
+const plane = new THREE.Mesh(geometry, material);
 
-scene.add(torus);
+scene.add(plane);
 
-//const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 function animate(){
   requestAnimationFrame(animate);
+
+  controls.update();
 
   renderer.render(scene, camera);
 }
